a890e193627a7506cd40b223ab9dcf982b172ffb
TeenAyers <mtmasi291@gmail.com>
2025-11-17 14:06:20 +0900

    環境リセット

--- Data/User.cs
+++ Data/User.cs
@@ -4,7 +4,12 @@ namespace _2025_employment_1.Models
 {
     public class User
     {
+        public int Id { get; set; }
 
+        [Required]
+        public string Username { get; set; }
 
+        [Required]
+        public string PasswordHash { get; set; }
     }
 }
